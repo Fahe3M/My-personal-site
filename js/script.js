50e5d6e034b45a26ee5e71d2d6bf327a391b89e2
Fahe3M <160449381+Fahe3M@users.noreply.github.com>
2024-02-23 16:17:45 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed ('.auto-type', {
+  strings : ['Front-End Developer', 'Python Programmer', 'Data Scientist'],
+  typeSpeed : 150,
+  backSpeed : 150,
+  loop : true
+});
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
